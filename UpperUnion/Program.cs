93837711e1e973fa5035e35d5395a8d2e5a4765d
Carolina Bastos <carol1999b@gmail.com>
2023-03-31 11:20:48 +0100

    Add UpperUnion

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // No arguments given
+            if(args.Length == 0)
+            {
+                Console.WriteLine("NO ARGUMENTS");
+            }
+            else
+            {
+                //Change all chars ToUpper()
+                for (int i = 0; i < args.Length; i++)
+                {
+                    args[i] = args[i].ToUpper();
+                }
+
+                Array.Sort(args);
+
+                StringBuilder final = new StringBuilder();
+                final.Append(String.Join("-", args));
+
+                Console.WriteLine(final);
+            }
+        }
+    }
+}
