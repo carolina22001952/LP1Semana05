0100ee79283902fddb18df645f2e1b3bbcc827fa
Carolina Bastos <carol1999b@gmail.com>
2023-03-31 10:52:25 +0100

    Show all dice numbers and sum

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -6,7 +6,29 @@ namespace RandomDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variables
+            int n;
+            int sum = 0;
+
+            Random random = new Random();
+
+            // Write instruction
+            Console.WriteLine("How many dice you want to role?");
+
+            // Get number o dice
+            n = int.Parse(Console.ReadLine());
+
+
+            // Show the number of each dice
+            for(int i = 0; i < n; i++)
+            {
+                int value = random.Next(1,7);
+                Console.WriteLine($"Dice number {i} = {value}");
+                sum += value;
+            }
+
+            // Show sum of all dice
+            Console.WriteLine($"Sum of all dice = {sum}.");
         }
     }
 }
